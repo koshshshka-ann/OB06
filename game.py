314727f67a1a30e56109d536117dd5c1770d1569
Anna Siryak <grafova.a.i@gmail.com>
2025-05-30 22:40:46 +0300

    Update game.py
    добавила код программы

--- game.py
+++ game.py
@@ -22,3 +22,57 @@
 # - Компьютер (computer), экземпляр класса Hero
 # Методы:
 # - start(): начинает игру, чередует ходы игрока и компьютера, пока один из героев не умрет. Выводит информацию о каждом ходе (кто атаковал и сколько здоровья осталось у противника) и объявляет победителя.
+
+class Hero:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.attack_power = 20
+
+    def attack(self, other):
+        other.health -= self.attack_power
+
+    def is_alive(self):
+        return self.health > 0
+
+
+class Game:
+    def __init__(self, player_name):
+        self.player = Hero(player_name)
+        self.computer = Hero("Компьютер")
+
+    def start(self):
+        print("Игра 'Битва героев' начинается!")
+        print(f"{self.player.name} (Здоровье: 100) vs {self.computer.name} (Здоровье: 100)")
+
+        current_attacker = self.player  # Первым атакует игрок
+
+        while self.player.is_alive() and self.computer.is_alive():
+            print("\n" + "=" * 30)
+
+            # Атака
+            defender = self.computer if current_attacker == self.player else self.player
+            current_attacker.attack(defender)
+
+            # Вывод информации об атаке
+            print(f"{current_attacker.name} атакует {defender.name}!")
+            print(f"У {defender.name} осталось {defender.health} здоровья")
+
+            # Смена хода
+            current_attacker = self.computer if current_attacker == self.player else self.player
+
+        # Определение победителя
+        print("\n" + "=" * 30)
+        if self.player.is_alive():
+            print(f"{self.player.name} побеждает!")
+        else:
+            print(f"{self.computer.name} побеждает!")
+        print("=" * 30)
+
+
+# Запуск игры
+if __name__ == "__main__":
+    name = input("Введите имя вашего героя: ")
+    game = Game(name)
+    game.start()
+    
